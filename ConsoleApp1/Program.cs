978407df665d0dd998304e4a9243769ac31bae33
s28185 <s28185@pjwstk.edu.pl>
2024-03-08 15:08:41 +0100

    Added getmax function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello, World!");
 int[] arr = new[] { 1, 2, 3, 4, 5 };
 
 Console.WriteLine(GetAvg(arr));
+Console.WriteLine(GetMax(arr));
 
 static double GetAvg(int[] arr)
 {
@@ -14,4 +15,17 @@ static double GetAvg(int[] arr)
     }
 
     return (double) sum / arr.Length;
+}
+
+static int GetMax(int[] arr)
+{
+    int max = 0;
+    foreach (var num in arr)
+    {
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    return max;
 }
